c325fd729126dba7f20561c09d3e4a6a22e55601
NotSandyFromKentucky <98691941+NotSandyFromKentucky@users.noreply.github.com>
2024-11-25 23:56:59 +0100

    %USERPROFILE% on registry now

--- set_onedrivefolder.bat
+++ set_onedrivefolder.bat
@@ -89,35 +89,35 @@ echo All specified folders have been processed.
 
 :: Set the registry keys
 echo Setting registry keys...
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "Desktop" /t REG_EXPAND_SZ /d "%iCloudDrivePath%\Desktop" /f
-echo Set registry key for Desktop to: %iCloudDrivePath%\Desktop
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "Desktop" /t REG_EXPAND_SZ /d "%%USERPROFILE%%\iCloudDrive\Desktop" /f
+echo Set registry key for Desktop to: %USERPROFILE%\iCloudDrive\Desktop
 
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "My Pictures" /t REG_EXPAND_SZ /d "%iCloudDrivePath%\Pictures" /f
-echo Set registry key for My Pictures to: %iCloudDrivePath%\Pictures
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "My Pictures" /t REG_EXPAND_SZ /d "%%USERPROFILE%%\iCloudDrive\Pictures" /f
+echo Set registry key for My Pictures to: %USERPROFILE%\iCloudDrive\Pictures
 
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "Personal" /t REG_EXPAND_SZ /d "%iCloudDrivePath%\Documents" /f
-echo Set registry key for Personal (Documents) to: %iCloudDrivePath%\Documents
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "Personal" /t REG_EXPAND_SZ /d "%%USERPROFILE%%\iCloudDrive\Documents" /f
+echo Set registry key for Personal (Documents) to: %USERPROFILE%\iCloudDrive\Documents
 
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "My Video" /t REG_EXPAND_SZ /d "%iCloudDrivePath%\Videos" /f
-echo Set registry key for My Video to: %iCloudDrivePath%\Videos
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "My Video" /t REG_EXPAND_SZ /d "%%USERPROFILE%%\iCloudDrive\Videos" /f
+echo Set registry key for My Video to: %USERPROFILE%\iCloudDrive\Videos
 
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "{374DE290-123F-4565-9164-39C4925E467B}" /t REG_EXPAND_SZ /d "%iCloudDrivePath%\Downloads" /f
-echo Set registry key for Downloads to: %iCloudDrivePath%\Downloads
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "{374DE290-123F-4565-9164-39C4925E467B}" /t REG_EXPAND_SZ /d "%%USERPROFILE%%\iCloudDrive\Downloads" /f
+echo Set registry key for Downloads to: %USERPROFILE%\iCloudDrive\Downloads
 
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "{339719B5-8C47-4894-94C2-D8F77ADD44A6}" /t REG_EXPAND_SZ /d "%iCloudDrivePath%\Images" /f
-echo Set registry key for Images to: %iCloudDrivePath%\Images
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "{339719B5-8C47-4894-94C2-D8F77ADD44A6}" /t REG_EXPAND_SZ /d "%%USERPROFILE%%\iCloudDrive\Images" /f
+echo Set registry key for Images to: %USERPROFILE%\iCloudDrive\Images
 
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "{EDC0FE71-98D8-4F4A-B920-C8DC133CB165}" /t REG_EXPAND_SZ /d "%iCloudDrivePath%\Videos\Captures" /f
-echo Set registry key for Videos (Captures) to: %iCloudDrivePath%\Videos\Captures
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "{EDC0FE71-98D8-4F4A-B920-C8DC133CB165}" /t REG_EXPAND_SZ /d "%%USERPROFILE%%\iCloudDrive\Videos\Captures" /f
+echo Set registry key for Videos (Captures) to: %USERPROFILE%\iCloudDrive\Videos\Captures
 
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "{F42EE2D3-909F-4907-8871-4C22FC0BF756}" /t REG_EXPAND_SZ /d "%iCloudDrivePath%\Documents" /f
-echo Set registry key for Documents to: %iCloudDrivePath%\Documents
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "{F42EE2D3-909F-4907-8871-4C22FC0BF756}" /t REG_EXPAND_SZ /d "%%USERPROFILE%%\iCloudDrive\Documents" /f
+echo Set registry key for Documents to: %USERPROFILE%\iCloudDrive\Documents
 
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "{0DDD015D-B06C-45D5-8C4C-F59713854639}" /t REG_EXPAND_SZ /d "%iCloudDrivePath%\Images" /f
-echo Set registry key for Images (again) to: %iCloudDrivePath%\Images
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "{0DDD015D-B06C-45D5-8C4C-F59713854639}" /t REG_EXPAND_SZ /d "%%USERPROFILE%%\iCloudDrive\Images" /f
+echo Set registry key for Images (again) to: %USERPROFILE%\iCloudDrive\Images
 
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "{35286A68-3C57-41A1-BBB1-0EAE73D76C95}" /t REG_EXPAND_SZ /d "%iCloudDrivePath%\Videos" /f
-echo Set registry key for Videos (again) to: %iCloudDrivePath%\Videos
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "{35286A68-3C57-41A1-BBB1-0EAE73D76C95}" /t REG_EXPAND_SZ /d "%%USERPROFILE%%\iCloudDrive\Videos" /f
+echo Set registry key for Videos (again) to: %USERPROFILE%\iCloudDrive\Videos
 
 echo.
 echo All folders created and registry keys set successfully. Please restart your computer for changes to take effect.
